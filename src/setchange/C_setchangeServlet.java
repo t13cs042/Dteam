ad90aef4cf0758a75dd588f10458085448a01dca
t13cs045 <t13cs045@yamanashi.ac.jp>
2016-01-08 15:50:15 +0900

    test

--- src/setchange/C_setchangeServlet.java
+++ src/setchange/C_setchangeServlet.java
@@ -1,7 +1,16 @@
 package setchange;
 
+import setchange.Memo;
+
 import java.io.IOException;
+import java.util.List;
+
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
 import javax.servlet.http.*;
+
+import setchange.LoginDB;
+import setchange.PMF;
 
 @SuppressWarnings("serial")
 public class C_setchangeServlet extends HttpServlet {
@@ -10,4 +19,92 @@ public class C_setchangeServlet extends HttpServlet {
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
 	}
+	
+	public void doPost( HttpServletRequest req, HttpServletResponse resp ) throws IOException
+	{
+
+		// jsp からのデータ
+		String	[] inputData	= new String[3]; // beforeMail と afterMail1 と afterMail2
+
+		// 登録エラー用のフラグ
+		int error = 0;
+		
+		// pm を用意
+		PersistenceManager	pm	= PMF.get().getPersistenceManager();
+		//Queryを用意
+		Query query = pm.newQuery(LoginDB.class);
+		
+		try{
+			inputData[0] = req.getParameter("before");
+			inputData[1] = req.getParameter("after1");
+			inputData[2] = req.getParameter("after2");
+
+			// ID欄が入力されているかチェック
+			if(inputData[0].equals("")){
+				error += 16;
+			}
+			//else if(!inputData[0].matches("[a-zA-Z0-9@.]+")){
+			else if(!inputData[0].matches("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$")){
+				error += 2;
+			}
+			else{
+				// 既に同じIDが使われていないかチェック
+				try{
+					// 検索、見付からなかったら例外を吐く
+					//pm.getObjectById(LoginDB.class, inputData[0]);
+					query.setFilter("mail == " + inputData[0]);
+					List<LoginDB> db =  (List<LoginDB>)pm.newQuery(query).execute();
+					if( db.isEmpty() )
+						error += 4;
+				}catch(Exception e){}
+			
+			
+			}
+			
+			// afterMailが入力されているかチェック
+			if(inputData[1].equals("")){
+				error +=32;
+			}
+			
+			if(inputData[2].equals("")){
+				error += 64;
+			}
+
+			if( !inputData[1].equals(inputData[2]) ){
+				error += 1;
+			}
+			
+			// IDが使用済なら登録不可
+			if(error != 0){
+				resp.sendRedirect("/settingchange/change_address.jsp?Error=" + String.valueOf(error)
+						+ "&Id=" + inputData[0] + "&Pass=" + inputData[1]);
+			}else{
+
+				
+
+				// データ変更
+				query.setFilter("mail ==" + inputData[0]);
+				
+				List<LoginDB> db =  (List<LoginDB>)pm.newQuery(query).execute();
+				db.get(0).setMail(inputData[1]);
+				
+
+				// 登録
+				//pm.makePersistent(data);
+				
+				
+				
+				// 画面を更新
+				resp.sendRedirect( "/index.html" );
+
+			}
+		}finally{
+			pm.close();
+		}
+
+		// 画面を更新
+	//	resp.sendRedirect( "/index.html" );
+	}
+	
+	
 }
