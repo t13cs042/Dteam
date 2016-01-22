b312a2c07ffd133e0c541f24d450eae1ee8f2241
nishina <t13cs042@yamanashi.ac.jp>
2016-01-22 15:11:45 +0900

    Comment.javaのテストケースを追加

---
+++ src/NewData/NewCommentTest.java
@@ -0,0 +1,53 @@
+package NewData;
+
+
+import static org.junit.Assert.*;
+
+import javax.jdo.PersistenceManager;
+
+import org.junit.Test;
+
+import Dataclass.Comment;
+import Dataclass.PMF;
+
+public class NewCommentTest {
+
+	@Test
+	public void testcheckerror() {
+		NewComment com = new NewComment();
+		assertEquals(com.checkerror("1", "2"), 0);//エラーなし
+		assertEquals(com.checkerror("", "a"), 1);//件名が入力されていない
+		assertEquals(com.checkerror("a", ""), 2);//本文が入力されていない
+		
+		String str = "";
+		for(int i = 0; i < 50; i++){
+			str+= "a";
+		}
+		
+		assertEquals(com.checkerror(str, "a"), 0);//件名の50文字制限のチェック(50文字の場合)
+		
+		str += "a";
+		
+		assertEquals(com.checkerror(str, "a"), 4);//件名の50文字制限のチェック(51文字の場合)
+		
+		String str2 = "";
+		for(int i = 0; i < 500; i++){
+			str2 += "a";
+		}
+		
+		assertEquals(com.checkerror("a", str2), 0);//本文の500文字制限のチェック(500文字の場合)
+		
+		str2 += "a";
+		
+		assertEquals(com.checkerror("a", str2), 8);//本文の500文字制限のチェック(501文字の場合)
+			
+	}
+	
+	@Test
+	public void test() {
+		Comment comment = new Comment("", "", "", "");
+
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		pm.makePersistent(comment);
+	}
+}
