d5a1c67b67f1bfdcfabb76335b0a9cefed4dad07
nishina <t13cs042@yamanashi.ac.jp>
2016-01-28 14:43:50 +0900

    新規登録のエラー処理を修正

--- src/signup_/Newsignup.java
+++ src/signup_/Newsignup.java
@@ -19,28 +19,28 @@ public class Newsignup extends HttpServlet {
 		String familyname = req.getParameter("familyname");
 		String firstname = req.getParameter("firstname");
 
-		String mail = req.getParameter("mail");
-		String password = req.getParameter("password");
-		String pppassword = req.getParameter("pppassword");
-		String area = req.getParameter("area");
-		String start_month = req.getParameter("start_month");
-		String finish_month = req.getParameter("finish_month");
-		String question1 = req.getParameter("question1");
-		String answer1 = req.getParameter("answer1");
-		String question2 = req.getParameter("question2");
-		String answer2 = req.getParameter("answer2");
+		String mail = (String)req.getParameter("mail");
+		String password = (String)req.getParameter("password");
+		String pppassword = (String)req.getParameter("pppassword");
+		String area = (String)req.getParameter("area");
+		String start_month = (String)req.getParameter("start_month");
+		String finish_month = (String)req.getParameter("finish_month");
+		String question1 = (String)req.getParameter("question1");
+		String answer1 = (String)req.getParameter("answer1");
+		String question2 = (String)req.getParameter("question2");
+		String answer2 = (String)req.getParameter("answer2");
 
 		resp.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = resp.getWriter();
-		
+
 		int error = 0;
 		PersistenceManager	pm	= PMF.get().getPersistenceManager();
-		
+
 		String query = "select from " + LoginDB.class.getName();
 
 		@SuppressWarnings("unchecked")
 		List<LoginDB> users = (List<LoginDB>) pm.newQuery(query).execute();
-		
+
 		for(LoginDB user: users){
 			if(mail.equals( user.getMail() ) ){
 				error += 16;
@@ -53,7 +53,6 @@ public class Newsignup extends HttpServlet {
 		if(firstname.equals("")){
 			error += 2;
 		}
-		
 		if(mail.equals("")){
 			error += 4;
 		}else if(!mail.matches("^[a-zA-Z0-9!#$%&'_`/=~\\*\\+\\-\\?\\^\\{\\|\\}]+(\\.[a-zA-Z0-9!#$%&'_`/=~\\*\\+\\-\\?\\^\\{\\|\\}]+)*+(.*)@[a-zA-Z0-9][a-zA-Z0-9\\-]*(\\.[a-zA-Z0-9\\-]+)+$")){
@@ -70,15 +69,13 @@ public class Newsignup extends HttpServlet {
 		if(!password.equals(pppassword)){
 			error += 256;
 		}
-		
+
 		int error_2 = 0;
 		if(area.equals("")){
 			error_2 += 1;
-		}
-		if(Integer.parseInt(area) > 10000 && Integer.parseInt(area) > 0){
+		}else if(Integer.parseInt(area) > 10000 || Integer.parseInt(area) < 0){
 			error_2 += 8;
 		}
-		
 		if(start_month.equals("") || finish_month.equals("")){
 			error_2 += 2;
 		}else if(Integer.parseInt(start_month) > Integer.parseInt(finish_month)){
@@ -88,20 +85,18 @@ public class Newsignup extends HttpServlet {
 			if(Integer.parseInt(finish_month) - Integer.parseInt(start_month) > 6)
 				error_2 += 4;	
 		}
-		
+
 		if(question1.equals("")){
-			if(!answer1.equals(""))
-				error_2 += 16;
-		}else{
-			if(answer1.equals(""))
-				error_2 += 32;
+			error_2 += 16;
+		}	
+		if(answer1.equals("")){
+			error_2 += 32;
 		}
 		if(question2.equals("")){
-			if(!answer2.equals(""))
-				error_2 += 64;
-		}else{
-			if(answer2.equals(""))
-				error_2 += 128;
+			error_2 += 64;
+		}
+		if(answer2.equals("")){
+			error_2 += 128;
 		}		
 		if(question1.length() > 500 || question2.length() > 500){
 			error_2 += 256;
@@ -119,7 +114,7 @@ public class Newsignup extends HttpServlet {
 					finish_month,  question1,  answer1,  question2, answer2,0);
 			try {
 				pm.makePersistent(logindb);
-			
+
 			} finally {
 				pm.close();
 			}
@@ -127,13 +122,13 @@ public class Newsignup extends HttpServlet {
 			out.println("<a href=\"/Login/login.jsp\">戻る</a>");
 		}
 	}
-	
+
 	public boolean isNumber(String num) {
-	    try {
-	        Integer.parseInt(num);
-	        return true;
-	        } catch (NumberFormatException e) {
-	        return false;
-	    }
+		try {
+			Integer.parseInt(num);
+			return true;
+		} catch (NumberFormatException e) {
+			return false;
+		}
 	}
 }
